e081428553d85b81e2f5b76070a031bef5106467
Lavdrim <sadikilavdrim109gmail.com>
2021-02-11 20:43:35 +0100

    krijimi i component not found dhe home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import {ElementsModule} from './elements/elements.module';
 import {CollectionsModule} from './collections/collections.module';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NotFoundComponentComponent,
   ],
   imports: [
     BrowserModule,
