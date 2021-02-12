ad8529b2606adc43553c59394260287ba4e4999e
Lavdrim <sadikilavdrim109gmail.com>
2021-02-12 01:18:57 +0100

    Regullmi i linqeve qe mos te dalin 2 linqe njekohesisht te aktivizuara edhe lazy loadin per collection dhe elements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import {HomeComponent} from './home/home.component';
 import {NotFoundComponentComponent} from './not-found-component/not-found-component.component';
 
 const routes: Routes = [
+  {path: 'collections',
+  loadChildren: () => import('./collections/collections.module').then(m => m.CollectionsModule)},
+  { path: 'elements',
+  loadChildren: () => import('./elements/elements.module').then(m => m.ElementsModule)},
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponentComponent }
 ];
